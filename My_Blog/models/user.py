ecfceaf3554c8c39a1042d56af5e8cf31224fc92
MAA <matykhinand2021@gmail.com>
2023-03-09 00:12:01 +0300

    DZ 5 compleate

--- My_Blog/models/user.py
+++ My_Blog/models/user.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, Boolean
+from ..security import flask_bcrypt
+from sqlalchemy import Column, String, Boolean, LargeBinary
 from .database import db
 from flask_login import UserMixin
 import uuid
@@ -7,10 +8,23 @@ import uuid
 class User(db.Model, UserMixin):
     id = Column(String(length=36), default=lambda: str(uuid.uuid4()), primary_key=True)
     username = Column(String(80), unique=True, nullable=False)
-    email = Column(String(150), unique=True, nullable=False)
+    first_name = Column(String(120), unique=False, nullable=False, default="", server_default="")
+    last_name = Column(String(120), unique=False, nullable=False, default="", server_default="")
+    email = Column(String(255), unique=True, nullable=False, default="", server_default="")
     avatar = Column(String(150), default='/static/img/default_avatar.jpg')
     is_staff = Column(Boolean, nullable=False, default=False)
-
+    _password = Column(LargeBinary, nullable=False)
 
     def __repr__(self):
         return f"<User #{self.id} {self.username!r}>"
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        self._password = flask_bcrypt.generate_password_hash(value)
+
+    def validate_password(self, password) -> bool:
+        return  flask_bcrypt.check_password_hash(self._password, password)
